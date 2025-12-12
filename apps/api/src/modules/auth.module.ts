56d2d6b939f409dbc45ce3b192fd45179ee52fca
huu.nguyen <nvhuu1998@gmail.com>
2025-12-12 21:42:57 +0700

    refactor: adjust relative import paths for JwtStrategy and LocalStrategy

--- apps/api/src/modules/auth.module.ts
+++ apps/api/src/modules/auth.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '../../src/infrastructure/auth/strategies/jwt.strategy';
-import { LocalStrategy } from '../../src/infrastructure/auth/strategies/local.strategy';
 import { AuthService } from '../application/services/auth.service';
+import { JwtStrategy } from '../infrastructure/auth/strategies/jwt.strategy';
+import { LocalStrategy } from '../infrastructure/auth/strategies/local.strategy';
 import { AuthController } from '../presentation/controllers/auth.controller';
 import { UsersModule } from './users.module';
 
